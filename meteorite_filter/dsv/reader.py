f9fc0419bf1c1cb9313bc2698af281fcf05c9d37
Nick Manos <nmanos@student.bridgew.edu>
2023-10-01 00:47:01 -0400

    Format source files

--- meteorite_filter/dsv/reader.py
+++ meteorite_filter/dsv/reader.py
@@ -8,36 +8,41 @@ class DSVReader:
             self._newline = '\n'
         else:
             self._newline = newline
-    
+
     def __del__(self):
         self._file.close()
 
     @property
     def line_num(self):
         return self._line_num
-    
+
     def __iter__(self):
         return self
-    
+
     def __next__(self) -> list[str]:
         self._line_num += 1
         return next(self._file).rstrip(self._newline).split(self.delimiter)
 
 class DSVDictReader(DSVReader):
-    def __init__(self, dsv_path: str, delimiter=',', fieldnames: list[str] | None=None, type_map: dict | None=None):
+    def __init__(
+            self,
+            dsv_path: str,
+            delimiter=',',
+            fieldnames: list[str] | None=None,
+            type_map: dict | None=None):
         super().__init__(dsv_path, delimiter)
 
         if fieldnames is None:
             self._fieldnames = super().__next__()
         else:
             self._fieldnames = fieldnames
-        
+
         self._type_map = type_map
-    
+
     @property
     def fieldnames(self):
         return self._fieldnames
-    
+
     def __next__(self) -> dict:
         row_dict = dict(zip(self._fieldnames, super().__next__()))
 
