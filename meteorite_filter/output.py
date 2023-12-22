4344fd79dd275930dd83f3f3f974b7beafac912b
Nick Manos <nmanos@student.bridgew.edu>
2023-12-22 18:38:14 -0500

    Replace print_table function in filter_data.py with TerminalOutput class implementing OutputInterface

--- meteorite_filter/output.py
+++ meteorite_filter/output.py
@@ -1,4 +1,15 @@
+from tui.table import TablePrinter
+
 class OutputInterface:
     @staticmethod
     def output(data: list[dict], field: str):
         pass
+
+
+class TerminalOutput(OutputInterface):
+    @staticmethod
+    def output(data: list[dict], field: str):
+        from constants import FILTER_OPTIONS
+        table = TablePrinter(('', 'NAME', FILTER_OPTIONS[field]['header']), [(row_no, row['name'], row[field]) for row_no, row in enumerate(data, 1)])
+        print()
+        print(table)
