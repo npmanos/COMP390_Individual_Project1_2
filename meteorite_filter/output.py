91c754b7a5224aa2673233d9e53349841f4dc5c8
Nick Manos <nmanos@student.bridgew.edu>
2023-12-22 18:39:14 -0500

    Create TextFileOutput class implementing OutputInterface

--- meteorite_filter/output.py
+++ meteorite_filter/output.py
@@ -1,3 +1,5 @@
+from datetime import datetime as dt
+from dsv.writer import DSVDictWriter
 from tui.table import TablePrinter
 
 class OutputInterface:
@@ -13,3 +15,14 @@ class TerminalOutput(OutputInterface):
         table = TablePrinter(('', 'NAME', FILTER_OPTIONS[field]['header']), [(row_no, row['name'], row[field]) for row_no, row in enumerate(data, 1)])
         print()
         print(table)
+
+
+class TextFileOutput(OutputInterface):
+    @staticmethod
+    def output(data: list[dict], field: str):
+        path = dt.now().strftime('%Y-%m-%d_%H_%M_%f.txt')
+        fieldnames = list(data[0].keys())
+
+        writer = DSVDictWriter(path, fieldnames, delimiter='\t')
+        writer.writeheader()
+        writer.writerows(data)
